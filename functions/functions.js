fb32713cb19b1ff84c1d02671cec1d7ac5fea014
lacey washington <lacey.m.washington@gmail.com>
2023-01-23 13:57:51 -0600

    added messages for if and statements confused about if and statements

--- functions/functions.js
+++ functions/functions.js
@@ -6,4 +6,10 @@ const errorCallback = (error) => {
     console.error(error);
 };
 
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback); 
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+message1 = confirm('Do you think space is dope?')
+
+message2 = confirm('DOPE WE LIKE SPACE TOO! Did you want to check out my ugly website about space?')
+
+message3 = prompt("are you old enough to be visting site unsupervised")
