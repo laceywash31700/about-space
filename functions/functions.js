0b4142859c5ba1336f9e177c780e9580ea3e78f8
lacey washington <lacey.m.washington@gmail.com>
2023-01-24 17:38:19 -0600

    exprimite for js

--- functions/functions.js
+++ functions/functions.js
@@ -1,15 +1,33 @@
-const successCallback = (position) => {
-    console.log(position);
-};
+// function should confirm if user thinks outer Space is dope (m1) 
+// if true function should run (m2) which confirms if they want to see the site.
+// else if function should confirm user to should leave site (m4)
+// if (m4) is true should redirect to google.
+// else if should confirm (m2)
+// if true (m2) is true should run age= prompt "how old are you?"
+// should input value as a number
+//  if (age <  12) site should open
+// else take user to cartoon network.
+let m1 = confirm('Do you think space is dope?');
+let m2 = confirm('DOPE WE LIKE SPACE TOO! Did you want to check out my ugly website about space?');
+let m3 = age = prompt("how old are you?");
+let m4 = confirm('get out then');
 
-const errorCallback = (error) => {
-    console.error(error);
-};
+if (m1 === true) {  
+    return m2
+}
+ else if (m1 === false) {
+    return m4
+}
 
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+else if (m4 === false) { 
+    return m2
+}
+else if (m2 === false) { 
+    return m4
+}
+else if (m2 === true) { 
+    return m3
 
-message1 = confirm('Do you think space is dope?')
-
-message2 = confirm('DOPE WE LIKE SPACE TOO! Did you want to check out my ugly website about space?')
-
-message3 = prompt("are you old enough to be visting site unsupervised")
+}
+else if (m3 < 12 ){ alert("no way jose?"); 
+}
